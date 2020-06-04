b6587a83c434e1143070ea733d39902ac08c9fda
BrunoMichelotti <brunomichelotti@hotmail.com>
2020-06-04 11:11:50 -0300

    Initial Commit: Estrutura Inicial do Projeto

---
+++ weekly-popcorn/weekly-popcorn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weekly-popcorn
+//
+//  Created by Usuario Local on 6/4/20.
+//  Copyright © 2020 everis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
